d8a82d508d5f3b37767cf8751d8858be0875d95b
Kavish Ambani <kavishkkambani@gmail.com>
2025-04-09 02:36:32 +0530

    bug fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Admin from "./pages/Admin";
 import Owners from "./pages/Owner";
 import Students from "./pages/Student";
 import './index.css';
-import Properties from "./pages/properties";
+import OwnerProperties from "./pages/Properties";
 function App() {
   return (
     <Router>
@@ -11,7 +11,7 @@ function App() {
         <Route path="/" element={<Admin />} />
         <Route path="/admin/owners" element={<Owners />} />
         <Route path="/admin/students" element={<Students />} />
-        <Route path="/admin/properties" element={<Properties />} />
+        <Route path="/admin/properties" element={<OwnerProperties />} />
 
       </Routes>
     </Router>
